6a0558f7a5c9e12b8f683fcb67e8c52b02f2a29b
lacapsule <info@lacapsule.org>
2022-03-30 15:38:26 +0200

    MAJ

---
+++ boutons.js
@@ -0,0 +1,22 @@
+
+
+let bulles = document.getElementById("boutonBulles");
+let noms = document.getElementById("boutonNoms");
+let toutesBulles = document.getElementById("b13c0333-102d-4ea1-b7d6-4ce164979cfc");
+let tousNoms = document.getElementById("bac92131-e0ac-4952-b465-08aa343a6732");
+bulles.addEventListener("click", () => {
+  if(getComputedStyle(toutesBulles).display != "none"){
+    toutesBulles.style.display = "none";
+  } else {
+    toutesBulles.style.display = "block";
+  }
+})
+
+function togg(){
+  if(getComputedStyle(tousNoms).display != "none"){
+    tousNoms.style.display = "none";
+  } else {
+    tousNoms.style.display = "block";
+  }
+};
+noms.onclick = togg;
