8e21b36f0bc0f0719b00775dc116cf57da5f0ac0
lacapsule <info@lacapsule.org>
2022-04-03 00:21:44 +0200

    MAJ

--- boutons.js
+++ boutons.js
@@ -1,8 +1,10 @@
 
 let bulles = document.getElementById("boutonBulles");
 let noms = document.getElementById("boutonNoms");
+let fonds = document.getElementById("boutonFonds");
 let toutesBulles = document.getElementById("b13c0333-102d-4ea1-b7d6-4ce164979cfc");
 let tousNoms = document.getElementById("bac92131-e0ac-4952-b465-08aa343a6732");
+let tousFonds = document.getElementById("b8e18138-5697-4033-9611-05eec396f769")
 bulles.addEventListener("click", () => {
   if(getComputedStyle(toutesBulles).display != "none"){
     toutesBulles.style.display = "none";
@@ -10,7 +12,6 @@ bulles.addEventListener("click", () => {
     toutesBulles.style.display = "block";
   }
 })
-
 function invisible(){
   if(getComputedStyle(tousNoms).display != "none"){
     tousNoms.style.display = "none";
@@ -19,3 +20,14 @@ function invisible(){
   }
 };
 noms.onclick = invisible;
+fonds.addEventListener("click", () => {
+  tousFonds.style.fill = textInput.value;
+  } 
+)
+
+textInput.addEventListener("keyup", () => {
+  if (event.keyCode === 13) {
+    tousFonds.style.fill = textInput.value;
+  }
+});
+
