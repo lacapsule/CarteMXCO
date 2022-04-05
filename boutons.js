2742f97fb7433254a1f259c1e1e903334f449feb
lacapsule <info@lacapsule.org>
2022-04-05 12:41:02 +0200

    MAJ

--- boutons.js
+++ boutons.js
@@ -1,20 +1,19 @@
-
+let couleurs = document.getElementById("couleurToutes");
 let toutesBulles = document.getElementById("b13c0333-102d-4ea1-b7d6-4ce164979cfc");
 let tousNoms = document.getElementById("bac92131-e0ac-4952-b465-08aa343a6732");
 let toutesVilles = document.getElementById("b8e18138-5697-4033-9611-05eec396f769");
 boutonBulles.addEventListener("click", () => {
-  if(getComputedStyle(toutesBulles, tousNoms).display != "none"){
+  if(getComputedStyle(tousNoms, toutesBulles).display != "none"){
     tousNoms.style.display = "none";
     toutesBulles.style.display = "none";
   } else {
-    toutesBulles.style.display = "block";
     tousNoms.style.display = "block";
+    toutesBulles.style.display = "block";
   }
-})
-
-couleurToutes.addEventListener("input", () => {
-  toutesVilles.style.fill = couleurToutes.value;
+});
+couleurs.addEventListener("input", () => {
+  toutesVilles.style.fill = couleurs.value;
   } 
-)
+);
   
 
