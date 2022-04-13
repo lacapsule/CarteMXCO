a4ad05c5aab5623c8837e53363b1877396803367
lacapsule <info@lacapsule.org>
2022-04-13 22:57:45 +0200

    MAJ

--- boutons.js
+++ boutons.js
@@ -3,6 +3,26 @@ let fond = document.getElementById("fond");
 let toutesBulles = document.getElementById("b13c0333-102d-4ea1-b7d6-4ce164979cfc");
 let tousNoms = document.getElementById("bac92131-e0ac-4952-b465-08aa343a6732");
 let boutonBulles = document.getElementById("boutonBulles");
+let boutonGeneral = document.getElementById("boutonMenuGeneral");
+let asideGeneral = document.getElementById("optionGeneral");
+let boutonCommunes = document.getElementById("boutonMenuCommunes");
+let asideCommunes = document.getElementById("optionCommunes");
+
+boutonGeneral.addEventListener("click", () => {
+  if(getComputedStyle(optionGeneral).display != "none"){
+    asideGeneral.style.display = "none";
+  } else {
+    asideGeneral.style.display = "flex";
+  }
+});
+
+boutonCommunes.addEventListener("click", () => {
+  if(getComputedStyle(optionCommunes).display != "none"){
+    asideCommunes.style.display = "none";
+  } else {
+    asideCommunes.style.display = "flex";
+  }
+});
 
 boutonBulles.addEventListener("click", () => {
   if(getComputedStyle(tousNoms, toutesBulles).display != "none"){
@@ -30,4 +50,3 @@ boutons.addEventListener("click", () => {
   }
 })
   
-
