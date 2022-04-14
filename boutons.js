a5ecbf0dcec75b0457d66b5b0661e006c5c3464e
lacapsule <info@lacapsule.org>
2022-04-15 01:52:05 +0200

    MAJ

--- boutons.js
+++ boutons.js
@@ -1,12 +1,18 @@
 let toutesVilles = document.getElementById("b8e18138-5697-4033-9611-05eec396f769");
+let couleurFrontieres = document.getElementById("b8e18138-5697-4033-9611-05eec396f769");
 let fond = document.getElementById("fond");
+let fondBulles = document.getElementById("b13c0333-102d-4ea1-b7d6-4ce164979cfc");
+let contoursBulles = document.getElementById("b13c0333-102d-4ea1-b7d6-4ce164979cfc");
 let toutesBulles = document.getElementById("b13c0333-102d-4ea1-b7d6-4ce164979cfc");
 let tousNoms = document.getElementById("bac92131-e0ac-4952-b465-08aa343a6732");
+let couleurNoms = document.getElementById("bac92131-e0ac-4952-b465-08aa343a6732");
 let boutonBulles = document.getElementById("boutonBulles");
 let boutonGeneral = document.getElementById("boutonMenuGeneral");
 let asideGeneral = document.getElementById("optionGeneral");
 let boutonCommunes = document.getElementById("boutonMenuCommunes");
 let asideCommunes = document.getElementById("optionCommunes");
+let boutonCouleursBulles = document.getElementById("boutonMenuBulles");
+let asideBulles = document.getElementById("optionBulles");
 
 boutonGeneral.addEventListener("click", () => {
   if(getComputedStyle(optionGeneral).display != "none"){
@@ -24,6 +30,14 @@ boutonCommunes.addEventListener("click", () => {
   }
 });
 
+boutonCouleursBulles.addEventListener("click", () => {
+  if(getComputedStyle(optionBulles).display != "none"){
+    asideBulles.style.display = "none";
+  } else {
+    asideBulles.style.display = "flex";
+  }
+});
+
 boutonBulles.addEventListener("click", () => {
   if(getComputedStyle(tousNoms, toutesBulles).display != "none"){
     tousNoms.style.display = "none";
@@ -33,20 +47,30 @@ boutonBulles.addEventListener("click", () => {
     toutesBulles.style.display = "block";
   }
 });
-couleurToutes.addEventListener("input", () => {
-  toutesVilles.style.fill = couleurToutes.value;
-  } 
-)
+
 couleurFond.addEventListener("input", () => {
   fond.style.backgroundColor = couleurFond.value;
   } 
 )
-
-boutons.addEventListener("click", () => {
-  if(getComputedStyle(boutons).bgColor != "#46b313"){
-    boutons.style.bgColor = "#46b313";
-  } else {
-    boutons.style.bgColor = "#46b313";
-  }
+couleurToutes.addEventListener("input", () => {
+  toutesVilles.style.fill = couleurToutes.value;
+  } 
+)
+couleurBulles.addEventListener("input", () => {
+  fondBulles.style.fill = couleurBulles.value;
+  } 
+)
+couleurContoursBulles.addEventListener("input", () => {
+  contoursBulles.style.stroke = couleurContoursBulles.value;
+  } 
+)
+couleurTextes.addEventListener("input", () => {
+  couleurNoms.style.fill = couleurTextes.value;
 })
+
+couleurToutesFrontieres.addEventListener("input", () => {
+  couleurFrontieres.style.stroke = couleurToutesFrontieres.value;
+  } 
+)
+
   
---
+++ boutonsBulles.js
@@ -0,0 +1,131 @@
+let bulleCouleurPlouneourMenez = document.getElementById("bf15d8a7-db89-4003-9fe0-f8bc838b9df0");
+let bulleCouleurPleyberChrist = document.getElementById("a761eac0-45d6-45e3-9093-84dfea3a1b9d");
+let bulleCouleurSaintThegonnec = document.getElementById("a4db58b7-9c30-4499-80bb-306a9e921a03");
+let bulleCouleurSaintSeve = document.getElementById("a03fc445-917e-4c55-9733-26b7731d0c88");
+let bulleCouleurTaule = document.getElementById("fc428f4a-9f0e-4791-8b2b-d58beda6675c");
+let bulleCouleurHenvic = document.getElementById("b76d895a-b4f9-40b4-82fa-5c7eba672cd7");
+let bulleCouleurCarantec = document.getElementById("a7721bfd-96aa-4916-afd5-7a270928f28c");
+let bulleCouleurSaintMartin = document.getElementById("a91099eb-4c3c-4534-b3d5-a3f02763041a");
+let bulleCouleurMorlaix = document.getElementById("e7b1665b-5d3c-4933-ba04-c10b38b61b1a");
+let bulleCouleurLocquenole = document.getElementById("e109832d-6605-4daa-8a5d-753a58fae0be");
+let bulleCouleurPlourin = document.getElementById("a8ac7e5e-8901-48e2-8ce7-fad0417168a5");
+let bulleCouleurLeCloitre = document.getElementById("aaccdea4-f016-4e2e-9733-6c2fa9bb1a81");
+let bulleCouleurPlougonven = document.getElementById("bb0eee94-b1d8-4975-a96c-85b69bf55501");
+let bulleCouleurLanneanou = document.getElementById("f85f2cf3-1186-49d6-b2f6-d3b609b47ae9");
+let bulleCouleurPlouigneau = document.getElementById("f9a1ee28-3065-4e3f-a4ba-95c4be19407b");
+let bulleCouleurGarlan = document.getElementById("fde93a90-83d1-422d-b76f-e9988aed6b2d");
+let bulleCouleurPlouezoch = document.getElementById("b2ea5c4d-55b8-4df9-976e-42e7b5a5974f");
+let bulleCouleurBotsorhel = document.getElementById("f5a37bd7-e9e6-4fca-8858-fa04a9dd3f7a");
+let bulleCouleurGuerlesquin = document.getElementById("aacdadee-131a-4650-85b7-a8a384477da0");
+let bulleCouleurMoysan = document.getElementById("a45c04a1-f2af-4c71-8405-4702cebe33d4");
+let bulleCouleurGuerrand = document.getElementById("be15baa7-4eab-477b-aad3-0b5f8a91d193");
+let bulleCouleurLanmeur = document.getElementById("a71fefbf-3f8c-4433-9d8c-87c00d7165b0");
+let bulleCouleurGuimaec = document.getElementById("ad1a97d2-08f9-4a65-8130-79a92f004e23");
+let bulleCouleurLocquirec = document.getElementById("fa8bfd84-7194-4cd0-b79d-ce7e7ca04a9a");
+let bulleCouleurSaintJean = document.getElementById("efa4db02-4a68-4b51-a6a7-507d464221f9");
+let bulleCouleurPlougasnou = document.getElementById("e759bd59-459b-4583-ab4e-5c4cc022c8d0");
+
+couleurBullePlouneour.addEventListener("input", () => {
+    bullePlouneourMenez.style.fill = couleurBullePlouneour.value;
+    tailleBullePlouneour.style.backgroundColor = couleurBullePlouneour.value;
+    })
+  couleurBullePleyber.addEventListener("input", () => {
+    bullePleyber.style.fill = couleurBullePleyber.value;
+    tailleBullePleyber.style.backgroundColor = couleurBullePleyber.value;
+    })
+  couleurBulleSaintTheg.addEventListener("input", () => {
+    bulleSaintTheg.style.fill = couleurBulleSaintTheg.value;
+    tailleBulleSaintTheg.style.backgroundColor = couleurBulleSaintTheg.value;
+    })
+  couleurBulleSaintSeve.addEventListener("input", () => {
+    bulleSainteSeve.style.fill = couleurBulleSaintSeve.value;
+    tailleBulleSaintSeve.style.backgroundColor = couleurBulleSaintSeve.value;
+    })
+  couleurBulleTaule.addEventListener("input", () => {
+    bulleTaule.style.fill = couleurBulleTaule.value;
+    tailleBulleTaule.style.backgroundColor = couleurBulleTaule.value;
+    })
+  couleurBulleHenvic.addEventListener("input", () => {
+    bulleHenvic.style.fill = couleurBulleHenvic.value;
+    tailleBulleHenvic.style.backgroundColor = couleurBulleHenvic.value;
+    })
+  couleurBulleCarantec.addEventListener("input", () => {
+    bulleCarantec.style.fill = couleurBulleCarantec.value;
+    tailleBulleCarantec.style.backgroundColor = couleurBulleCarantec.value;
+    })
+  couleurBulleSaintMartin.addEventListener("input", () => {
+    bulleSaintMartin.style.fill = couleurBulleSaintMartin.value;
+    tailleBulleSaintMartin.style.backgroundColor = couleurBulleSaintMartin.value;
+    })
+  couleurBulleMorlaix.addEventListener("input", () => {
+    bulleMorlaix.style.fill = couleurBulleMorlaix.value;
+    tailleBulleMorlaix.style.backgroundColor = couleurBulleMorlaix.value;
+    })
+  couleurBulleLocquenole.addEventListener("input", () => {
+    bulleLocquenole.style.fill = couleurBulleLocquenole.value;
+    tailleBulleLocquenole.style.backgroundColor = couleurBulleLocquenole.value;
+    })
+   couleurBullePlourin.addEventListener("input", () => {
+    bullePlourin.style.fill = couleurBullePlourin.value;
+    tailleBullePlourin.style.backgroundColor = couleurBullePlourin.value;
+    })
+  couleurBulleLeCloitre.addEventListener("input", () => {
+    bulleLeCloitre.style.fill = couleurBulleLeCloitre.value;
+    tailleBulleLeCloitre.style.backgroundColor = couleurBulleLeCloitre.value;
+    })
+  couleurBullePlougonven.addEventListener("input", () => {
+    bullePlougonven.style.fill = couleurBullePlougonven.value;
+    tailleBullePlougonven.style.backgroundColor = couleurBullePlougonven.value;
+    })
+  couleurBulleLanneanou.addEventListener("input", () => {
+    bulleLanneanou.style.fill = couleurBulleLanneanou.value;
+    tailleBulleLanneanou.style.backgroundColor = couleurBulleLanneanou.value;
+    })
+  couleurBullePlouigneau.addEventListener("input", () => {
+    bullePlouigneau.style.fill = couleurBullePlouigneau.value;
+    tailleBullePlouigneau.style.backgroundColor = couleurBullePlouigneau.value;
+    })
+  couleurBulleGarlan.addEventListener("input", () => {
+    bulleGarlan.style.fill = couleurBulleGarlan.value;
+    tailleBulleGarlan.style.backgroundColor = couleurBulleGarlan.value;
+    })
+  couleurBullePlouezoch.addEventListener("input", () => {
+    bullePlouezoch.style.fill = couleurBullePlouezoch.value;
+    tailleBullePlouezoch.style.backgroundColor = couleurBullePlouezoch.value;
+    })
+  couleurBulleBotsorhel.addEventListener("input", () => {
+    bulleBotsorhel.style.fill = couleurBulleBotsorhel.value;
+    tailleBulleBotsorhel.style.backgroundColor = couleurBulleBotsorhel.value;
+    })
+  couleurBulleGuerlesquin.addEventListener("input", () => {
+    bulleGuerlesquin.style.fill = couleurBulleGuerlesquin.value;
+    tailleBulleGuerlesquin.style.backgroundColor = couleurBulleGuerlesquin.value;
+    })
+  couleurBulleMoysan.addEventListener("input", () => {
+    bulleMoysan.style.fill = couleurBulleMoysan.value;
+    tailleBulleMoysan.style.backgroundColor = couleurBulleMoysan.value;
+    })
+  couleurBulleGuerrand.addEventListener("input", () => {
+    bulleGuerrand.style.fill = couleurBulleGuerrand.value;
+    tailleBulleGuerrand.style.backgroundColor = couleurBulleGuerrand.value;
+    })
+  couleurBulleLanmeur.addEventListener("input", () => {
+    bulleLanmeur.style.fill = couleurBulleLanmeur.value;
+    tailleBulleLanmeur.style.backgroundColor = couleurBulleLanmeur.value;
+    })
+  couleurBulleGuimaec.addEventListener("input", () => {
+      bulleGuimaec.style.fill = couleurBulleGuimaec.value;
+      tailleBulleGuimaec.style.backgroundColor = couleurBulleGuimaec.value;
+    })
+  couleurBulleLocquirec.addEventListener("input", () => {
+    bulleLocquirec.style.fill = couleurBulleLocquirec.value;
+    tailleBulleLocquirec.style.backgroundColor = couleurBulleLocquirec.value;
+    })
+  couleurBulleSaintJean.addEventListener("input", () => {
+    bulleSaintJean.style.fill = couleurBulleSaintJean.value;
+    tailleBulleSaintJean.style.backgroundColor = couleurBulleSaintJean.value;
+    })
+  couleurBullePlougasnou.addEventListener("input", () => {
+    bullePlougasnou.style.fill = couleurBullePlougasnou.value;
+    tailleBullePlougasnou.style.backgroundColor = couleurBullePlougasnou.value;
+    });
