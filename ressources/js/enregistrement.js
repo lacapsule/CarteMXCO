46139f1f6a6294ba5ac816ff3a4cf4bf87348a03
lacapsule <info@lacapsule.org>
2022-06-15 16:23:11 +0200

    Suppression de scripts inutiles, modifications mobile, suppression de la fonction print et modifications du menu

--- ressources/js/enregistrement.js
+++
@@ -1,6 +0,0 @@
-function download() {
-    boutonMenuEnregistrer.addEventListener("click", () => {
-    var downloading = browser.downloads.download("#imprimer")
-    var dl = document.getElementById('#imprimer');
-    dl.src = "carteMorlaixCommunaute.png";
-})}
