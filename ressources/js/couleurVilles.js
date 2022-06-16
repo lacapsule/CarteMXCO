821f8d1d5c36051f40a80e15be2391f09804c231
Alexandre Harel <mcfly@mywebsoluce.fr>
2022-06-16 21:20:44 +0200

    update couleur des villes

--- ressources/js/couleurVilles.js
+++ ressources/js/couleurVilles.js
@@ -1,131 +1,17 @@
-let couleurVillePlouneourMenez = document.getElementById("ad8ce1cf-d7bf-4e6a-8a01-1381f5bedaab");
-let couleurVillePleyberChrist = document.getElementById("f67bab1d-f4dd-4f82-a9cf-1f03da6055cc");
-let couleurVilleSaintThegonnec = document.getElementById("b72b22dd-8b5d-48e2-9a31-ade134b628f4");
-let couleurVilleSaintSeve = document.getElementById("e65c9bae-f9c5-4dac-a8aa-34a15a6012c0");
-let couleurVilleTaule = document.getElementById("bc446247-e92f-4903-b995-ca414dda1396");
-let couleurVilleHenvic = document.getElementById("ee8323d8-0ff8-49a7-9f68-be914d95516d");
-let couleurVilleCarantec = document.getElementById("b1478828-1576-4b59-ad35-0de024f348cc");
-let couleurVilleSaintMartin = document.getElementById("f8bb002d-efde-4956-ad6f-86c552439a83");
-let couleurVilleMorlaix = document.getElementById("bfd61b45-0d71-4b6d-9dd9-5ea7e9c920c4");
-let couleurVilleLocquenole = document.getElementById("fc6ca94b-ec6c-445c-be29-3e26ed36be18");
-let couleurVillePlourin = document.getElementById("b9293e63-f983-4c87-8546-cc078ce15509");
-let couleurVilleLeCloitre = document.getElementById("f4876ba0-75e0-4f79-9a2b-6bbd78075fdb");
-let couleurVillePlougonven = document.getElementById("a2a7cd6f-59dc-429c-972e-e9504ff8c960");
-let couleurVilleLanneanou = document.getElementById("af2dc544-982c-4e1d-8b05-f8b33da9ce85");
-let couleurVillePlouigneau = document.getElementById("af01d8cb-db7e-4a7f-abd0-a0a25b9a3146");
-let couleurVilleGarlan = document.getElementById("a35271df-8a6a-49e0-86e6-502423d7c931");
-let couleurVillePlouezoch = document.getElementById("a7f5976f-6ce6-4a95-bade-1ec28bcf3a1f");
-let couleurVilleBotsorhel = document.getElementById("eeca40ee-0584-4ea1-9253-2e2562856737");
-let couleurVilleGuerlesquin = document.getElementById("e7ffc33b-529f-497d-95a8-de5d17f543f6");
-let couleurVilleMoysan = document.getElementById("ab41bafd-5fce-43cf-9036-8160aa5288f5");
-let couleurVilleGuerrand = document.getElementById("fce91cc1-7f02-4894-933f-1911a31101f1");
-let couleurVilleLanmeur = document.getElementById("bdb52ed4-5d70-47a8-ba84-807496ae4970");
-let couleurVilleGuimaec = document.getElementById("b0e22cd6-8e74-49f6-a06a-b336e9c6896f");
-let couleurVilleLocquirec = document.getElementById("bd83b1b3-42cf-403f-a79a-f2455337aba5");
-let couleurVilleSaintJean = document.getElementById("e3b68945-2e34-4c9c-83a3-3158a252e9b2");
-let couleurVillePlougasnou = document.getElementById("b476af40-0d89-400f-9515-14ddd604b8be");
+const btns = document.querySelectorAll('.color');
+const bgs = document.querySelectorAll('.fond');
 
-  couleurPlouneour.addEventListener("input", () => {
-    couleurVillePlouneourMenez.style.fill = couleurPlouneour.value;
-    boutonPlouneour.style.backgroundColor = couleurPlouneour.value;
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        var name = btn.dataset.value;   
+            bgs.forEach(fond => {
+                if(fond.dataset.name == btn.dataset.value) {
+                    btn.addEventListener("input", () => {
+                        fond.style.fill = btn.value;
+                        fond.classList.add("set");
+                    });
+                }
+            });
+
     });
-  couleurPleyber.addEventListener("input", () => {
-    couleurVillePleyberChrist.style.fill = couleurPleyber.value;
-    boutonPleyber.style.backgroundColor = couleurPleyber.value;
-    });
-  couleurSaintTheg.addEventListener("input", () => {
-    couleurVilleSaintThegonnec.style.fill = couleurSaintTheg.value;
-    boutonSaintTheg.style.backgroundColor = couleurSaintTheg.value;
-    });
-  couleurSaintSeve.addEventListener("input", () => {
-    couleurVilleSaintSeve.style.fill = couleurSaintSeve.value;
-    boutonSaintSeve.style.backgroundColor = couleurSaintSeve.value;
-    });
-  couleurTaule.addEventListener("input", () => {
-    couleurVilleTaule.style.fill = couleurTaule.value;
-    boutonTaule.style.backgroundColor = couleurTaule.value;
-    });
-  couleurHenvic.addEventListener("input", () => {
-    couleurVilleHenvic.style.fill = couleurHenvic.value;
-    boutonHenvic.style.backgroundColor = couleurHenvic.value;
-    });
-  couleurCarantec.addEventListener("input", () => {
-    couleurVilleCarantec.style.fill = couleurCarantec.value;
-    boutonCarantec.style.backgroundColor = couleurCarantec.value;
-    });
-  couleurSaintMartin.addEventListener("input", () => {
-    couleurVilleSaintMartin.style.fill = couleurSaintMartin.value;
-    boutonSaintMartin.style.backgroundColor = couleurSaintMartin.value;
-    });
-  couleurMorlaix.addEventListener("input", () => {
-    couleurVilleMorlaix.style.fill = couleurMorlaix.value;
-    boutonMorlaix.style.backgroundColor = couleurMorlaix.value;
-    });
-  couleurLocquenole.addEventListener("input", () => {
-    couleurVilleLocquenole.style.fill = couleurLocquenole.value;
-    boutonLocquenole.style.backgroundColor = couleurLocquenole.value;
-    });
-   couleurPlourin.addEventListener("input", () => {
-    couleurVillePlourin.style.fill = couleurPlourin.value;
-    boutonPlourin.style.backgroundColor = couleurPlourin.value;
-    });
-  couleurLeCloitre.addEventListener("input", () => {
-    couleurVilleLeCloitre.style.fill = couleurLeCloitre.value;
-    boutonLeCloitre.style.backgroundColor = couleurLeCloitre.value;
-    });
-  couleurPlougonven.addEventListener("input", () => {
-    couleurVillePlougonven.style.fill = couleurPlougonven.value;
-    boutonPlougonven.style.backgroundColor = couleurPlougonven.value;
-    });
-  couleurLanneanou.addEventListener("input", () => {
-    couleurVilleLanneanou.style.fill = couleurLanneanou.value;
-    boutonLanneanou.style.backgroundColor = couleurLanneanou.value;
-    });
-  couleurPlouigneau.addEventListener("input", () => {
-    couleurVillePlouigneau.style.fill = couleurPlouigneau.value;
-    boutonPlouigneau.style.backgroundColor = couleurPlouigneau.value;
-    });
-  couleurGarlan.addEventListener("input", () => {
-    couleurVilleGarlan.style.fill = couleurGarlan.value;
-    boutonGarlan.style.backgroundColor = couleurGarlan.value;
-    });
-  couleurPlouezoch.addEventListener("input", () => {
-    couleurVillePlouezoch.style.fill = couleurPlouezoch.value;
-    boutonPlouezoch.style.backgroundColor = couleurPlouezoch.value;
-    });
-  couleurBotsorhel.addEventListener("input", () => {
-    couleurVilleBotsorhel.style.fill = couleurBotsorhel.value;
-    boutonBotsorhel.style.backgroundColor = couleurBotsorhel.value;
-    });
-  couleurGuerlesquin.addEventListener("input", () => {
-    couleurVilleGuerlesquin.style.fill = couleurGuerlesquin.value;
-    boutonGuerlesquin.style.backgroundColor = couleurGuerlesquin.value;
-    });
-  couleurMoysan.addEventListener("input", () => {
-    couleurVilleMoysan.style.fill = couleurMoysan.value;
-    boutonMoysan.style.backgroundColor = couleurMoysan.value;
-    });
-  couleurGuerrand.addEventListener("input", () => {
-    couleurVilleGuerrand.style.fill = couleurGuerrand.value;
-    boutonGuerrand.style.backgroundColor = couleurGuerrand.value;
-    });
-  couleurLanmeur.addEventListener("input", () => {
-    couleurVilleLanmeur.style.fill = couleurLanmeur.value;
-    boutonLanmeur.style.backgroundColor = couleurLanmeur.value;
-    });
-  couleurGuimaec.addEventListener("input", () => {
-      couleurVilleGuimaec.style.fill = couleurGuimaec.value;
-      boutonGuimaec.style.backgroundColor = couleurGuimaec.value;
-    });
-  couleurLocquirec.addEventListener("input", () => {
-    couleurVilleLocquirec.style.fill = couleurLocquirec.value;
-    boutonLocquirec.style.backgroundColor = couleurLocquirec.value;
-    });
-  couleurSaintJean.addEventListener("input", () => {
-    couleurVilleSaintJean.style.fill = couleurSaintJean.value;
-    boutonSaintJean.style.backgroundColor = couleurSaintJean.value;
-    });
-  couleurPlougasnou.addEventListener("input", () => {
-    couleurVillePlougasnou.style.fill = couleurPlougasnou.value;
-    boutonPlougasnou.style.backgroundColor = couleurPlougasnou.value;
-    });;
+});
